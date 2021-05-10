1db5d8befc4f9fd3e1280c0223cd59b70742fbc4
Sushant571 <83909938+Sushant571@users.noreply.github.com>
2021-05-10 08:23:55 +0530

    Update App.py

--- Softwares/App.py
+++ Softwares/App.py
@@ -1,11 +1,1 @@
-def convert_even(number):
- if number % 2 is not 0 :
-    even_number = number+1
- else:
-     print("Number requested is already an even number")
-def convert_odd(number):
- if number % 2 is not 0 :
-    odd_number = number+1
- else:
-     print("Number requested is already an even number")
-convert_odd(2)
+
