765d73c66e0ab22450d138e91d563c07e27be104
Sushant <83909938+Sushant571@users.noreply.github.com>
2021-05-12 18:37:25 +0530

    Delete Algoritms directory

--- Algoritms/JsonReader.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open('students.json') as f:
-  data = json.load(f)
-print(data)
--- Algoritms/functions.py
+++
@@ -1,3 +0,0 @@
-def save_user(**user):
-    print(user)
-save_user(Id=173589,Name='Sushant',Age=12,School ='DPS Gurgaon')
